4b913e50b1e84f128468b3b7e94d3a7595ac6378
Brandon Williams <Brandonmatthew.williams@gmail.com>
2024-11-01 12:52:50 -0700

    Added Hello World practice

--- 11-01-2024/TemplateGames/Practice.py
+++ 11-01-2024/TemplateGames/Practice.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+name = input("What is your first name: ")
+
+# print(f"Hello {name}!")
+
+lastName = input("What is your last name: ")
+print(f"Your full name is {name} {lastName}!")
